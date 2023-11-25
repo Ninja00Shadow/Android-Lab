3be68ab6fc4e6d43fcec190a40868d2230490fd4
Dawid <mrshadow813@gmail.com>
2023-11-25 22:00:34 +0100

    correct message

--- AndroidLab3/app/src/main/java/com/example/androidlab3/CatFragment.kt
+++ AndroidLab3/app/src/main/java/com/example/androidlab3/CatFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.RadioGroup
+import android.widget.Switch
 
 class CatFragment : Fragment() {
 
@@ -29,9 +32,34 @@ class CatFragment : Fragment() {
         catSubmitButton?.setOnClickListener {
             Log.i("CatFragment","Cat button clicked")
             parentFragmentManager.setFragmentResult("messageFromChild", Bundle().apply {
-                putString("message", "Cat")
+                putString("message", createCatMessage())
             })
         }
+    }
+
+    private fun createCatMessage(): String {
+        val ribbed = when (view?.findViewById<Switch>(R.id.ribbedSwitch)?.isChecked) {
+            true -> "Ribbed"
+            else -> ""
+        }
+        val breed = when (view?.findViewById<RadioGroup>(R.id.radioGroup)?.checkedRadioButtonId) {
+            R.id.britishShorthairButton -> "British Shorthair"
+            R.id.egyptianButton -> "Egyptian Mau"
+            R.id.mixedButton -> "Mixed"
+            else -> "Unknown"
+        }
+        val furColor = when (view?.findViewById<CheckBox>(R.id.blackFurCheckBox)?.isChecked) {
+            true -> "Black"
+            else -> ""
+        } + " " + when (view?.findViewById<CheckBox>(R.id.brownFurCheckBox)?.isChecked) {
+            true -> "Brown"
+            else -> ""
+        } + " " + when (view?.findViewById<CheckBox>(R.id.yellowFurCheckBox)?.isChecked) {
+            true -> "Yellow"
+            else -> ""
+        }
+
+        return "Cat:\n $ribbed $breed with $furColor fur"
     }
 
     companion object {
--- AndroidLab3/app/src/main/java/com/example/androidlab3/DogFragment.kt
+++ AndroidLab3/app/src/main/java/com/example/androidlab3/DogFragment.kt
@@ -1,10 +1,14 @@
 package com.example.androidlab3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.RadioGroup
+import android.widget.Switch
 
 class DogFragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -13,12 +17,6 @@ class DogFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        view?.findViewById<View>(R.id.dogSubmitButton)?.setOnClickListener {
-            parentFragmentManager.setFragmentResult("messageFromChild", Bundle().apply {
-                putString("message", "Dog")
-            })
-        }
     }
 
     override fun onCreateView(
@@ -27,6 +25,42 @@ class DogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dogSubmitButton = view.findViewById<View>(R.id.dogSubmitButton)
+
+        dogSubmitButton?.setOnClickListener {
+            Log.i("CatFragment","Cat button clicked")
+            parentFragmentManager.setFragmentResult("messageFromChild", Bundle().apply {
+                putString("message", createDogMessage())
+            })
+        }
+    }
+
+    private fun createDogMessage(): String {
+        val name = view?.findViewById<android.widget.EditText>(R.id.dogNameEditText)?.text.toString()
+
+        val breed = when (view?.findViewById<RadioGroup>(R.id.radioGroup)?.checkedRadioButtonId) {
+            R.id.goldenRetrieverButton -> "Golden Retriever"
+            R.id.jamnikButton -> "Sausage Dog"
+            R.id.pitbulButton -> "Pitbul"
+            else -> "Unknown"
+        }
+        val furColor = when (view?.findViewById<CheckBox>(R.id.blackFurCheckBox)?.isChecked) {
+            true -> "Black"
+            else -> ""
+        } + " " + when (view?.findViewById<CheckBox>(R.id.brownFurCheckBox)?.isChecked) {
+            true -> "Brown"
+            else -> ""
+        } + " " + when (view?.findViewById<CheckBox>(R.id.yellowFurCheckBox)?.isChecked) {
+            true -> "Yellow"
+            else -> ""
+        }
+
+        return "Dog:\n $name $breed with $furColor fur"
     }
 
     companion object {
